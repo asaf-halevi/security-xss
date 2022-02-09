9a4bfed06325041ec2091f5c10ecf2ed47e8f776
Halevi, Asaf (ah864q) <ah864q@intl.att.com>
2022-02-09 17:24:59 +0200

    add images

---
+++ src/app/shared/material.module.ts
@@ -0,0 +1,75 @@
+import { ModuleWithProviders, NgModule } from '@angular/core';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatRippleModule } from '@angular/material/core';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { OverlayModule } from '@angular/cdk/overlay';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+
+const modules = [
+    MatDialogModule,
+    MatRadioModule,
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatDividerModule,
+    MatGridListModule,
+    MatInputModule,
+    MatMenuModule,
+    MatAutocompleteModule,
+    MatRippleModule,
+    MatChipsModule,
+    MatProgressBarModule,
+    MatSidenavModule,
+    MatExpansionModule,
+    MatListModule,
+    MatSlideToggleModule,
+    MatButtonToggleModule,
+    MatTabsModule,
+    OverlayModule,
+    MatProgressSpinnerModule,
+    MatTableModule,
+    MatSortModule
+];
+
+
+@NgModule({
+    imports: [
+        modules,
+    ],
+    exports: [
+        modules,
+    ],
+})
+
+export class MaterialModule {
+    static forRoot(): ModuleWithProviders<MaterialModule> {
+        return {
+            ngModule: MaterialModule,
+            providers: [],
+        };
+    }
+}
